0a7a58dffcd78e15c86be25847b5b3c27f847327
tumana777 <tumana991@gmail.com>
2024-02-16 23:47:59 +0400

    Done Challenge 3

---
+++ homework_18/main18_3.py
@@ -0,0 +1,19 @@
+'''
+დაწერეთ ტრანზაქციის ფუნქცია, რომელსაც გადაეცემა ატრიბუტად ბალანსი და გადასახდელი თანხა, დაუწერეთ დეკორატორი, 
+რომელიც საკომისიოს ჩამოაჭრის 1 ლარს და თუ საკმარისი თანხა არ იქნება ანგარიშზე დაუბრუნეთ შეცდომის ტექსტი
+'''
+def commision(func):
+    def wrapper(*args, **kwargs):
+        bal = args[0]
+        am = args[1]
+        if am + 1 <= bal:
+            return func(bal-1, am)
+        else:
+            return 'Insufficient balance'
+    return wrapper
+
+@commision
+def transaction(balance, amount):
+    return f'Your balance was ${balance + 1}, transaction is ${amount}, commision is ${1},so balance remaining ${balance - amount}.'
+
+print(transaction(21, 10))
