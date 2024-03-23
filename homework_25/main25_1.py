cceacd275b5520580951b197cf9c713b52141501
tumana777 <tumana991@gmail.com>
2024-03-23 14:49:59 +0400

    Done Homework 25

--- homework_25/main25_1.py
+++ homework_25/main25_1.py
@@ -8,37 +8,22 @@ import faker, json, threading
 # Create fake class
 fake = faker.Faker()
 
-# Create Data for json file
-json_data = {"data" : [{"id" : i, "name" : fake.name(),"country" : fake.country(),"email" : fake.email()} for i in range(1, 51)]}
-
-# Write data to json file
-with open("fake_data1.json", "w") as json_file:
-    json.dump(json_data, json_file, indent=4)
-
-# Re-Create Data for json file
-json_data = {"data" : [{"id" : i, "name" : fake.name(),"country" : fake.country(),"email" : fake.email()} for i in range(1, 51)]}
-
-# Write data to json file
-with open("fake_data2.json", "w") as json_file:
-    json.dump(json_data, json_file, indent=4)
-
-# Re-Create Data for json file
-json_data = {"data" : [{"id" : i, "name" : fake.name(),"country" : fake.country(),"email" : fake.email()} for i in range(1, 51)]}
-
-# Write data to json file
-with open("fake_data3.json", "w") as json_file:
-    json.dump(json_data, json_file, indent=4)
+# Function to create json files
+def create_json_file(file_name):
     
-# Re-Create Data for json file
-json_data = {"data" : [{"id" : i, "name" : fake.name(),"country" : fake.country(),"email" : fake.email()} for i in range(1, 51)]}
+    # Create Data for json file
+    json_data = {"data" : [{"id" : i, "name" : fake.name(),"country" : fake.country(),"email" : fake.email()} for i in range(1, 51)]}
 
-# Write data to json file
-with open("fake_data4.json", "w") as json_file:
-    json.dump(json_data, json_file, indent=4)
-    
+    # Write data to json file
+    with open(file_name, "w") as json_file:
+        json.dump(json_data, json_file, indent=4)
 
-
+# File names
 files = ["fake_data1.json", "fake_data2.json", "fake_data3.json", "fake_data4.json"]
+
+# Create json files
+for file in files:
+    create_json_file(file)
 
 # Handle and Read Data from json files
 def handling_data(file):
