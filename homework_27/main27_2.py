920ad92cf0cf512fb6dff269bc2f9f12a9eae2fa
tumana777 <tumana991@gmail.com>
2024-04-05 17:32:13 +0400

    Done Challenge 2

--- homework_27/main27_2.py
+++ homework_27/main27_2.py
@@ -1,15 +1,25 @@
 """
 დაწერეთ ასინქრონული ფუნქცია, რომელიც რენდომად არჩეული დროით დააყოვნებს და დაბეჭდავს რიცხვებს 1-დან 10-მდე
 """
-# Import modules
-import random, asyncio
+import asyncio, random
+from codetiming import Timer
 
-# Create async function
-async def print_numbers():
+async def print_numbers(number, delay):
+    timer = Timer(text=f"Printed: {number} -> in {{:.2f}} seconds")
+    timer.start()
+    print(f"Started function to print {number}")
+    await asyncio.sleep(delay)
+    timer.stop()
+
+async def main():
+    tasks = []
+    
     for i in range(1, 11):
-        rand_time = random.randint(1, 5)
-        await asyncio.sleep(rand_time)
-        print(f"{i} -> delay time {rand_time} sec.")
-        
-# Run Async Function
-asyncio.run(print_numbers())
+        number = f"Number {i}"
+        delay = random.randint(1, 5)
+        tasks.append(print_numbers(number, delay))
+    
+    all_tasks = asyncio.gather(*tasks)
+    await all_tasks
+
+asyncio.run(main())
